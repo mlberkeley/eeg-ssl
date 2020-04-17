55fdab60516c524c129b3d20ee84f93611b372d3
Raffi Hotter <rhotter@users.noreply.github.com>
2020-04-16 22:18:05 -0400

    added super

--- feature_extractor.py
+++ feature_extractor.py
@@ -15,7 +15,7 @@ class EEG_FeatureExtractor(nn.Module):
 		embedding_dim: embedding dimension (D)
 		"""
 		# input is (1, C, T) <-- notation (channels, dim1, dim2) is different than paper (dim1, dim2, channels)
-		
+		super().__init__()
 		self.depthwise_conv = nn.Conv2d(in_channels=1, out_channels=C, kernel_size=(C,1))
 		self.spatialwise_conv1 = nn.Conv2d(in_channels=1, out_channels=n_spatial_filters, kernel_size=(1,k))
 		self.spatialwise_conv2 = nn.Conv2d(in_channels=n_spatial_filters, out_channels=n_spatial_filters, kernel_size=(1,k))
