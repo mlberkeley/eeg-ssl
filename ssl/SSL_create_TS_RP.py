6311df3481dc65785b7a438dc40af109eb2de703
Ana Cismaru <anacismaru@berkeley.edu>
2020-05-19 14:29:10 -0700

    Fixed int bug

--- ssl/SSL_create_TS_RP.py
+++ ssl/SSL_create_TS_RP.py
@@ -9,8 +9,7 @@ from os import listdir
 from os.path import isfile, join
 from mne import preprocessing
 
-from PPD2 import PPD2
-from SSL_TS_RP import temporal_shuffling, relative_positioning
+from ssl.SSL_TS_RP import temporal_shuffling, relative_positioning
 
 def create(preprocessed, T_pos_RP, T_neg_RP, T_pos_TS, T_neg_TS):
     """ Runs the whole pipeline and saves Numpy datasets and labels.
@@ -43,14 +42,11 @@ def create(preprocessed, T_pos_RP, T_neg_RP, T_pos_TS, T_neg_TS):
             os.makedirs("SSL_TS/TS_labels")
         
         # temporal_shuffling(data, T_pos, T_neg):
-       # TS_dataset, TS_labels = temporal_shuffling(epoch_data, T_pos_TS, T_neg_TS)
+        TS_dataset, TS_labels = temporal_shuffling(epoch_data, T_pos_TS, T_neg_TS)
 
-        #print("\n\n\nTS dataset")
-        #print(TS_dataset)
-        
         # Save Dataset
-        #np.save('SSL_TS/TS_dataset/' + file_name, TS_dataset)
-        #np.save('SSL_TS/TS_labels/' + file_name, TS_labels)
+        np.save('SSL_TS/TS_dataset/' + file_name, TS_dataset)
+        np.save('SSL_TS/TS_labels/' + file_name, TS_labels)
 
         # If folder doesn't exist, then create it.
         if not os.path.isdir("SSL_RP"):
@@ -60,20 +56,19 @@ def create(preprocessed, T_pos_RP, T_neg_RP, T_pos_TS, T_neg_TS):
             os.makedirs("SSL_RP/RP_labels")
         
         ### relative_positioning(data, T_pos, T_neg):
-        RP_dataset, RP_labels = relative_positioning(epoch_data, T_neg_RP, T_neg_RP)
+        RP_dataset, RP_labels = relative_positioning(epoch_data, T_pos_RP, T_neg_RP)
         
-        print("\n\n\nRP dataset")
-        print(RP_dataset[0])
-
-        print("\n\n\nRP labels")
-        print(RP_labels)
-
         # Save Dataset
         np.save('SSL_RP/RP_dataset/' + file_name, RP_dataset)
         np.save('SSL_RP/RP_labels/' + file_name, RP_labels)
 
-#if __name__ == '__main__':
+if __name__ == '__main__':
     # Map command line arguments to function arguments.
-    #create(*sys.argv[1:])
+    preprocessed = sys.argv[1]
+    T_pos_RP = sys.argv[2]
+    T_neg_RP = sys.argv[3]
+    T_pos_TS = sys.argv[4]
+    T_neg_TS = sys.argv[5]
+    create(preprocessed, T_pos_RP, T_neg_RP, T_pos_TS, T_neg_TS)
 
 ### AAV :)
