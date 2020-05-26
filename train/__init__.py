c25e626851d47006b95033d97731b3cd88b52448
Raffi Hotter <rhotter@users.noreply.github.com>
2020-05-25 21:48:27 -0400

    updated init.py

--- train/__init__.py
+++ train/__init__.py
@@ -1,3 +1,3 @@
-from hand_engineered_baseline import hand_engineered_baseline
-from train_linear import train_linear
-from train_supervised_baseline import train_supervised_baseline
+from .hand_engineered_baseline import hand_engineered_baseline
+from .train_linear import train_linear
+from .train_supervised_baseline import train_supervised_baseline
