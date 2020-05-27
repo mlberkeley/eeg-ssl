3c9d18c998e1d079935efd28541ed428aa5ac051
Raffi Hotter <rhotter@users.noreply.github.com>
2020-05-26 20:08:58 -0400

    added scores

--- __init__.py
+++ __init__.py
@@ -1,3 +1,4 @@
 from .hand_engineered_baseline import hand_engineered_baseline
 from .train_linear import train_linear
 from .train_supervised_baseline import train_supervised_baseline
+from .evaluate import *
