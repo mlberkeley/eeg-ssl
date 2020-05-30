91ab00471a345137eadb99512a01a5fa54ce0e2c
alfredoav <andere@berkeley.edu>
2020-05-29 23:16:39 -0700

    finished get_item and temporal shuffling, needs debugging

--- ssl/EEG_SSL_RP_Dataset.py
+++ ssl/EEG_SSL_RP_Dataset.py
@@ -1,11 +1,5 @@
 import numpy as np
 import random
-#import torch
-#from torch import optim
-#from torch.utils import data
-
-#from train_helpers import normalize_one
-#from models import CPC_EEG
 
 import os
 import numpy as np
@@ -25,49 +19,75 @@ from torchvision import transforms, utils
 import warnings
 warnings.filterwarnings("ignore")
 
-plt.ion()
-
 from ssl.new_SSL_TS_RP import temporal_shuffling, relative_positioning
 from preprocessing.new_preprocess import preprocess
 
 
-class EEG_SSL_RP_Dataset(Dataset):
+class EEG_SSL_Dataset(Dataset):
 
-
-	def __init__(self, preprocessed, T_pos_RP, T_neg_RP,\
+	def __init__(self, data_folder, T_pos, T_neg,\
 	 			sampling_freq=100, window_length=30, predict_delay=60, batch_size=128):
 
-		self.T_pos_RP = int(T_pos_RP)
-		self.T_neg_RP = int(T_neg_RP)
-
+		self.data_folder = data_folder
+		self.T_pos = int(T_pos)
+		self.T_neg = int(T_neg)
 		self.batch_size = batch_size
 		self.window_length = window_length
 		self.predict_delay = predict_delay
 		self.sampling_freq = sampling_freq
+		self.files = [f for f in os.listdir(data_folder) if f.endswith("PSG.edf")]
+        self.preprocessed = []
+        for f in self.files:
+            full_path = os.path.join(data_folder, f)
+            pp_file = preprocess(full_path)
+            self.preprocessed.append(pp_file)
+
+        TS_dataset, TS_labels = temporal_shuffling(pp_file)
+        self.num_files = len(self.files)
+		self.num_epochs = TS_dataset.shape[0]
+        self.num_samples = 6
+
+	def __len__(self):
+        return self.num_files * self.num_epochs * self.num_samples
 
 
-	def get_RP_minibatch(data_folder, T_pos_RP, T_neg_RP, num_users, num_samples):
-	"""
+	def __getitem__(self, idx):
+		"""
 
-	"""
-	minibatch_RP = []
-	files = random.sample([f for f in os.listdir(data_folder) if f.endswith("PSG.edf")], int(num_users))
-	for f in files:
-		full_path = os.path.join(data_folder, f)
-		preprocessed = preprocess(full_path)
-		RP_dataset, RP_labels = relative_positioning(preprocessed, int(T_pos_RP), int(T_neg_RP), int(num_samples))
-		minibatch_RP.append((RP_dataset, RP_labels))
+		"""
+        ### determine where we will be sampling from the index
+        file_idx = idx // 6
+        epoch_idx = sub_idx // num_epochs
+        sample_idx = idx % 6
 
-	return minibatch_RP
+        ### Sampling with the indexes
+        f = self.preprocesseded[file_idx]
+        TS_dataset, TS_labels = temporal_shuffling(f, epoch_idx)
+        TS_dataset = TS_dataset[sample_idx]
+        TS_labels = TS_labels[sample_idx]
+
+        return TS_labels, TS_labels
 
 
-	def relative_positioning(epochs, T_pos, T_neg, num_samples):
+	def get_batch():
+		minibatch_TS = []
+		files = random.sample(self.files, int(num_users))
+		for f in files:
+			full_path = os.path.join(data_folder, f)
+
+			TS_dataset, TS_labels = temporal_shuffling(preprocessed)
+			minibatch_TS.append((TS_dataset, TS_labels))
+
+		return minibatch_TS
+
+
+	def relative_positioning(epochs, self.T_pos, self.T_neg, num_samples):
 	    """ Builds a self-supervised (relative positioning) dataset of epochs
 
 	    Args:
 	        epochs - Numpy datset of time-series arrays
-	        T_pos - positive context to sample from
-	        T_neg - negative context to sample from
+	        self.T_pos - positive context to sample from
+	        self.T_neg - negative context to sample from
 	        num_samples - int representing number of epochs to sample
 
 	    Output:
@@ -77,48 +97,49 @@ class EEG_SSL_RP_Dataset(Dataset):
 	            s - # of eeg channels in each sample
 	            c - Samples per channel = 30s * 128Hz
 	        TS_labels - Temporal Shuffling labels of dimensions (1, L)
-	            for each y = {1: if |sample1-sample2| < T_pos and -1: if |sample1-sample2| > T_neg}
+	            for each y = {1: if |sample1-sample2| < self.T_pos and -1: if |sample1-sample2| > self.T_neg}
 	    """
-	    total_samples = epochs.shape[0] * 6
-	    RP_dataset = np.empty((total_samples, 2, epochs.shape[1], 3867)) ##TODO change shape of dataset according to num_samples
-	    RP_labels = np.empty((total_samples, 1)) ##TODO change shape of dataset according to num_samples
-	    counter = 0
+        np.random.seed(0)
+        total_samples = self.num_samples
+        RP_dataset = np.empty((6, 3, epochs.shape[1], 3867))
+        RP_labels = np.empty((6, 1))
+        counter = 0
 
 	    #select random epoch and get index
 	    print("\n\n\n EPOCHS:")
 	    print(epochs)
 
-	    for _, idx in enumerate(np.random.randint(len(epochs), size = num_samples)):
-	        sample1 = epochs[idx]
-	        for _ in range(3): # Loop for T_pos
-	            sample2_index = np.random.randint(max(idx-T_pos, 0), min(idx+T_pos, epochs.shape[0]-1))
-	            while sample2_index == idx: # should not be the same
-	                sample2_index = np.random.randint(max(idx-T_pos, 0), min(idx+T_pos, epochs.shape[0]-1))
-	            sample2 = epochs[sample2_index]
 
-	            y = 1
+        sample1 = epochs[idx]
+        for _ in range(3): # Loop for self.T_pos
+            sample2_index = np.random.randint(max(idx-self.T_pos, 0), min(idx+self.T_pos, epochs.shape[0]-1))
+            while sample2_index == idx: # should not be the same
+                sample2_index = np.random.randint(max(idx-self.T_pos, 0), min(idx+self.T_pos, epochs.shape[0]-1))
+            sample2 = epochs[sample2_index]
 
-	            RP_sample = np.array([sample1, sample2])
-	            RP_dataset[counter] = RP_sample
-	            RP_labels[counter] = y
-	            counter += 1
+            y = 1
 
-	        for _ in range(3): # Loop for T_neg
-	            if idx-T_neg <= 0: # T_neg if (corners)
-	                sample2_index = np.random.randint(idx+T_neg, epochs.shape[0])
-	            elif idx+T_neg >= epochs.shape[0]: # take care of low == high
-	                sample2_index = np.random.randint(0, idx-T_neg)
-	            else:
-	                sample2_index_1 = np.random.randint(idx+T_neg, epochs.shape[0])
-	                sample2_index_2 = np.random.randint(0, idx-T_neg)
-	                sample2_index = list([sample2_index_1, sample2_index_2])[int(random.uniform(0,1))]
-	            sample2 = epochs[sample2_index]
+            RP_sample = np.array([sample1, sample2])
+            RP_dataset[counter] = RP_sample
+            RP_labels[counter] = y
+            counter += 1
 
-	            y = -1
+        for _ in range(3): # Loop for self.T_neg
+            if idx-self.T_neg <= 0: # self.T_neg if (corners)
+                sample2_index = np.random.randint(idx+self.T_neg, epochs.shape[0])
+            elif idx+self.T_neg >= epochs.shape[0]: # take care of low == high
+                sample2_index = np.random.randint(0, idx-self.T_neg)
+            else:
+                sample2_index_1 = np.random.randint(idx+self.T_neg, epochs.shape[0])
+                sample2_index_2 = np.random.randint(0, idx-self.T_neg)
+                sample2_index = list([sample2_index_1, sample2_index_2])[int(random.uniform(0,1))]
+            sample2 = epochs[sample2_index]
 
-	            RP_sample = np.array([sample1, sample2])
-	            RP_dataset[counter] = RP_sample
-	            RP_labels[counter] = y
-	            counter += 1
+            y = -1
+
+            RP_sample = np.array([sample1, sample2])
+            RP_dataset[counter] = RP_sample
+            RP_labels[counter] = y
+            counter += 1
 
 	    return RP_dataset, RP_label
--- ssl/EEG_SSL_TS_Dataset.py
+++ ssl/EEG_SSL_TS_Dataset.py
@@ -1,16 +1,37 @@
 import numpy as np
 import random
 
+import os
+import numpy as np
+import sys
+from os import listdir
+from os.path import isfile, join
+
+import torch
+import pandas as pd
+from skimage import io, transform
+import numpy as np
+import matplotlib.pyplot as plt
+from torch.utils.data import Dataset, DataLoader
+from torchvision import transforms, utils
+
+# Ignore warnings
+import warnings
+warnings.filterwarnings("ignore")
+
+from ssl.new_SSL_TS_RP import temporal_shuffling, relative_positioning
+from preprocessing.new_preprocess import preprocess
+
 
 
 class EEG_SSL_Dataset(Dataset):
 
-	def __init__(self, data_folder, T_pos_TS, T_neg_TS,\
+	def __init__(self, data_folder, T_pos, T_neg,\
 	 			sampling_freq=100, window_length=30, predict_delay=60, batch_size=128):
 
 		self.data_folder = data_folder
-		self.T_pos_TS = int(T_pos_TS)
-		self.T_neg_TS = int(T_neg_TS)
+		self.T_pos = int(T_pos)
+		self.T_neg = int(T_neg)
 		self.batch_size = batch_size
 		self.window_length = window_length
 		self.predict_delay = predict_delay
@@ -23,10 +44,12 @@ class EEG_SSL_Dataset(Dataset):
             self.preprocessed.append(pp_file)
 
         TS_dataset, TS_labels = temporal_shuffling(pp_file)
-		self.len = len(self.files) * TS_dataset.shape[0] * 6
+        self.num_files = len(self.files)
+		self.num_epochs = TS_dataset.shape[0]
+        self.num_samples = 6
 
 	def __len__(self):
-        return self.len
+        return self.num_files * self.num_epochs * self.num_samples
 
 
 	def __getitem__(self, idx):
@@ -34,18 +57,15 @@ class EEG_SSL_Dataset(Dataset):
 
 		"""
         ### determine where we will be sampling from the index
-        sub_idx = idx//6
+        file_idx = idx // 6
+        epoch_idx = sub_idx // num_epochs
+        sample_idx = idx % 6
 
-        file_num = self.preprocess[sub_idx//(len (self.preprocessed))]
-        file_epoch = file_num[sub_idx % len(self.preprocessed)]
-
-        file_option = idx % 6
-
-        ### Sampling with the numbers
-        f = self.preprocessed[file_num][file_epoch]
-        TS_dataset, TS_labels = temporal_shuffling(f, file_epoch)
-        TS_dataset = TS_dataset[file_option]
-        TS_labels = TS_labels[file_option]
+        ### Sampling with the indexes
+        f = self.preprocesseded[file_idx]
+        TS_dataset, TS_labels = temporal_shuffling(f, epoch_idx)
+        TS_dataset = TS_dataset[sample_idx]
+        TS_labels = TS_labels[sample_idx]
 
         return TS_labels, TS_labels
 
@@ -61,13 +81,14 @@ class EEG_SSL_Dataset(Dataset):
 
 		return minibatch_TS
 
+
     def temporal_shuffling(epochs, idx):
         """ Builds a self-supervised (temporal shuffling) dataset of epochs
 
         Args:
             epochs - Numpy datset of time-series arrays
-            T_pos - positive context to sample from
-            T_neg - negative context to sample from
+            self.T_pos - positive context to sample from
+            self.T_neg - negative context to sample from
             num_samples - int representing number of epochs to sample
 
         Output:
@@ -86,19 +107,19 @@ class EEG_SSL_Dataset(Dataset):
         counter = 0
 
         sample1 = epochs[idx]
-        for _ in range(3): # T_pos loop
-            sample2_index = np.random.randint(max(idx-T_pos, 0), min(idx+T_pos, epochs.shape[0]-1))
+        for _ in range(3): # self.T_pos loop
+            sample2_index = np.random.randint(max(idx-self.T_pos, 0), min(idx+self.T_pos, epochs.shape[0]-1))
             while sample2_index == idx: # should not be the same
-                sample2_index = np.random.randint(max(idx-T_pos, 0), min(idx+T_pos, epochs.shape[0]-1))
+                sample2_index = np.random.randint(max(idx-self.T_pos, 0), min(idx+self.T_pos, epochs.shape[0]-1))
             sample2 = epochs[sample2_index]
 
-            if idx-T_neg <= 0: # T_neg if (corners)
-                sample3_index = np.random.randint(idx+T_neg, epochs.shape[0])
-            elif idx+T_neg >= epochs.shape[0]: # take care of low == high
-                sample3_index = np.random.randint(0, idx-T_neg)
+            if idx-self.T_neg <= 0: # self.T_neg if (corners)
+                sample3_index = np.random.randint(idx+self.T_neg, epochs.shape[0])
+            elif idx+self.T_neg >= epochs.shape[0]: # take care of low == high
+                sample3_index = np.random.randint(0, idx-self.T_neg)
             else:
-                sample3_index_1 = np.random.randint(idx+T_neg, epochs.shape[0])
-                sample3_index_2 = np.random.randint(0, idx-T_neg)
+                sample3_index_1 = np.random.randint(idx+self.T_neg, epochs.shape[0])
+                sample3_index_2 = np.random.randint(0, idx-self.T_neg)
                 sample3_index = list([sample3_index_1, sample3_index_2])[int(random.uniform(0,1))]
             sample3 = epochs[sample3_index]
 
@@ -112,15 +133,15 @@ class EEG_SSL_Dataset(Dataset):
             TS_labels[counter] = y
             counter += 1
 
-        for _ in range(3): # T_neg loop
-            sample2_index = np.random.randint(max(idx-T_pos, 0), min(idx+T_pos, epochs.shape[0]-1))
+        for _ in range(3): # self.T_neg loop
+            sample2_index = np.random.randint(max(idx-self.T_pos, 0), min(idx+self.T_pos, epochs.shape[0]-1))
             while sample2_index == idx: # should not be the same
-                sample2_index = np.random.randint(max(idx-T_pos, 0), min(idx+T_pos, epochs.shape[0]-1))
+                sample2_index = np.random.randint(max(idx-self.T_pos, 0), min(idx+self.T_pos, epochs.shape[0]-1))
             sample2 = epochs[sample2_index]
 
-            sample3_index = np.random.randint(max(idx-T_pos, 0), min(idx+T_pos, epochs.shape[0]-1))
+            sample3_index = np.random.randint(max(idx-self.T_pos, 0), min(idx+self.T_pos, epochs.shape[0]-1))
             while sample2_index == sample3_index or sample3_index == idx: # should not be the same
-                sample3_index = np.random.randint(max(idx-T_pos, 0), min(idx+T_pos, epochs.shape[0]-1))
+                sample3_index = np.random.randint(max(idx-self.T_pos, 0), min(idx+self.T_pos, epochs.shape[0]-1))
             sample3 = epochs[sample3_index]
 
             if idx < sample2_index and sample2_index < sample3_index:
