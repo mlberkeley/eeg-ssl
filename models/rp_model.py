3b0472ed80979a4e318042bf070fa56db24ba95b
Raffi Hotter <rhotter@users.noreply.github.com>
2020-06-10 17:44:49 -0400

    p

--- models/rp_model.py
+++ models/rp_model.py
@@ -1,13 +1,15 @@
 import torch
 from torch import nn
 from .feature_extractor import EEG_FeatureExtractor
+import IPython
+e = IPython.embed
 
 class Relative_Positioning(nn.Module):
   def __init__(self, C, T, k=50, m=13, dropout_prob=0.5, embedding_dim=100, n_spatial_filters=8):
     super().__init__()
     self.feature_extractor = EEG_FeatureExtractor(C, T, k, m, dropout_prob, embedding_dim, n_spatial_filters).cuda()
     self.linear = nn.Linear(embedding_dim, 1)
-    self.loss_fn = nn.BCEWithLogitsLoss(reduction='none')
+    self.loss_fn = nn.BCEWithLogitsLoss()
 
   def forward(self, x):
     first_samples = x[:,0].unsqueeze(dim=1)
@@ -23,4 +25,7 @@ class Relative_Positioning(nn.Module):
   
   def loss(self, x, labels):
     out = self(x)
-    return self.loss_fn(out, labels)
+    loss = self.loss_fn(out, labels)
+    if loss.mean() < 0:
+      e()
+    return loss
