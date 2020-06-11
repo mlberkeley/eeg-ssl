6c663377d846d3d51413dc5dc4fbce429d05842d
Raffi Hotter <rhotter@users.noreply.github.com>
2020-06-10 22:49:29 -0400

    p

--- ssl/EEG_SSL_RP_Dataset.py
+++ ssl/EEG_SSL_RP_Dataset.py
@@ -24,17 +24,21 @@ warnings.filterwarnings("ignore")
 #from ssl.new_SSL_TS_RP import temporal_shuffling, relative_positioning
 from eegssl.preprocessing.new_preprocess import preprocess
 
+MINUTES_TO_SECONDS = 60
+
 class EEG_SSL_Dataset(Dataset):
-    def __init__(self, T_pos, T_neg, anchor_windows_per_recording=2000, raw_data_folder=None, preprocessed_file=None,
+    def __init__(self, T_pos, T_neg, anchor_windows_per_recording=2000,
+    samples_per_anchor_window=6,
+    raw_data_folder=None, preprocessed_file=None,
     save_preprocessed_path=None, sampling_freq=100,
     window_length=30, predict_delay=60):
         """
         Takes in either a data folder or a preprocessed file
         """
-        self.T_pos = int(T_pos)
-        self.T_neg = int(T_neg)
+        self.T_pos = np.floor(T_pos * MINUTES_TO_SECONDS / window_length) # convert units from minutes to # of time windows
+        self.T_neg = np.ceil(T_neg * MINUTES_TO_SECONDS / window_length)
         self.anchor_windows_per_recording = int(anchor_windows_per_recording)
-        self.window_length = window_length
+        self.window_length = window_length # in seconds
         self.predict_delay = predict_delay
         self.sampling_freq = sampling_freq
         if ((raw_data_folder is None and preprocessed_file is None)
@@ -55,29 +59,54 @@ class EEG_SSL_Dataset(Dataset):
             self.preprocessed, self.files = pickle.load(open(preprocessed_file, 'rb'))
 
         self.num_files = len(self.files)
-        self.num_samples = 6
+        self.samples_per_anchor_window = samples_per_anchor_window
 
     def __len__(self):
-        return self.num_files * self.anchor_windows_per_recording * self.num_samples
+        return self.num_files * self.anchor_windows_per_recording * self.samples_per_anchor_window
 
     def __getitem__(self, idx):
-        file_idx = idx // (self.num_samples * self.anchor_windows_per_recording)
-        sample_idx = idx % self.num_samples
+        file_idx = idx // (self.samples_per_anchor_window * self.anchor_windows_per_recording)
+        sample_idx = idx % self.samples_per_anchor_window
         f = self.preprocessed[file_idx]
-        epoch_idx = np.random.randint(len(f))
+        anchor_idx = np.random.randint(len(f))
 
         ### Sampling with the indexes
-        RP_sample, RP_label = self.relative_positioning(f, epoch_idx, sample_idx)
+        RP_sample, RP_label = self.relative_positioning(f, anchor_idx, sample_idx)
 
         return RP_sample, RP_label
     
-    def relative_positioning(self, recording, epoch_idx, sample_idx):
+    def sample_pos_idx(self, anchor_idx, recording_len):
+        """
+        sample positive sample index uniformly in the union of 2 intervals
+        (anchor_idx-self.T_pos, anchor_idx) u (anchor_idx, anchor_idx+self.T_pos)
+        """
+        left_interval_start = min(anchor_idx+self.T_pos, recording_len)
+        right_interval_end = max(anchor_idx-self.T_pos, 0)
+
+        random_idx = np.random.randint(left_interval_start + right_interval_end - 1)
+        left_interval_length = anchor_idx - left_interval_start
+        if random_idx > left_interval_length:
+            random_idx += 1
+        random_idx += anchor_idx-self.T_pos
+        return random_idx
+
+    def sample_neg_idx(self, anchor_idx, recording_len):
+        """
+        sample negative sample index uniformly in the union of 2 intervals
+        (0, anchor_idx-self.T_neg) U (anchor_idx+self.T_neg, recording_len)
+        """
+        random_idx = np.random.randint(recording_len-2*self.T_neg)
+        if random_idx > anchor_idx - self.T_neg:
+            random_idx += 2*self.T_neg
+        return random_idx
+
+    def relative_positioning(self, recording, anchor_idx, sample_idx):
         """ Retrives a self-supervised (relative positioning) sample
         Args:
             recording - Numpy datset of time-series arrays
-            self.T_pos - positive context to sample from
-            self.T_neg - negative context to sample from
-            num_samples - int representing number of epochs to sample
+            self.T_pos - positive context to sample from (in minutes)
+            self.T_neg - negative context to sample from (in minutes)
+            samples_per_anchor_window - int representing number of epochs to sample
         Output:
             RP_sample - Relative Positioning sample of dimensions (2, s, c)
                 2 - sample1 + sample2
@@ -86,30 +115,17 @@ class EEG_SSL_Dataset(Dataset):
             RP_labels - Relative Positioning label of dimensions (1)
                 for each y = {1: if |sample1-sample2| < self.T_pos and -1: if |sample1-sample2| > self.T_neg}
         """
-
-        # TODO: Ask Alfredo to explain what is going on here.
-        sample1 = recording[epoch_idx]
+        anchor_window = recording[anchor_idx]
         if sample_idx <= 2: # self.T_pos loop
-            # np.random.seed(sample_idx)
-            sample2_index = np.random.randint(max(epoch_idx-self.T_pos, 0), min(epoch_idx+self.T_pos, recording.shape[0]-1))
-            while sample2_index == epoch_idx: # should not be the same (TODO: could fix the previous line so this doesn't happen)
-                sample2_index = np.random.randint(max(epoch_idx-self.T_pos, 0), min(epoch_idx+self.T_pos, recording.shape[0]-1))
-            sample2 = recording[sample2_index]
+            pos_idx = self.sample_pos_idx(anchor_idx, len(recording))
+            pos_window = recording[pos_idx]
             y = np.array([1])
-            RP_sample = np.array([sample1, sample2])
+            RP_sample = np.array([anchor_window, pos_window])
             RP_label = y
         else: # Loop for self.T_neg
-            # np.random.seed(sample_idx)
-            if epoch_idx-self.T_neg <= 0: # self.T_neg if (corners)
-                sample2_index = np.random.randint(epoch_idx+self.T_neg, recording.shape[0])
-            elif epoch_idx+self.T_neg >= recording.shape[0]: # take care of low == high
-                sample2_index = np.random.randint(0, epoch_idx-self.T_neg)
-            else:
-                sample2_index_1 = np.random.randint(epoch_idx+self.T_neg, recording.shape[0])
-                sample2_index_2 = np.random.randint(0, epoch_idx-self.T_neg)
-                sample2_index = list([sample2_index_1, sample2_index_2])[int(random.uniform(0,1))] # why int(random.uniform(0,1))? That's always 0...
-            sample2 = recording[sample2_index]
+            neg_idx = self.sample_neg_idx(anchor_idx, len(recording))
+            neg_window = recording[neg_idx]
             y = np.array([-1])
-            RP_sample = np.array([sample1, sample2])
+            RP_sample = np.array([anchor_window, neg_window])
             RP_label = y
         return RP_sample, RP_label
